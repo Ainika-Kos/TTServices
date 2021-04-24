25a7de8799bf29f83e70a93f048f3c502a85dc2e
Ainika <aina.kostina@gmail.com>
2021-04-24 23:10:57 +0300

    Added camera icon to the card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons';
 import { CardType } from '../../data/types';
 import Button from '../button/button';
 import './card.scss';
@@ -18,18 +20,22 @@ const Card: FC<CardType> = ({
   icon }) => {
 
   const imageURL = `${process.env.PUBLIC_URL}/images/${image}.jpg`;
-  const iconURL = `${process.env.PUBLIC_URL}/images/${icon}.jpg`;
 
   return (
     
     <div className={`card ${category} ${background}`}>
-      {image.length > 0 ? (
+      {image ? (
         <img src={imageURL} className="card__image" alt={image} />) : ('')}
       <Button text={buttonText} buttonClass={buttonClass} />
       <div className="card__info-wrapper">
         <h1 className={`card__info__title ${titleClass}`}>{ title }</h1>
         <p className={`card__info__time ${timeClass}`}>{time}</p>
       </div>
+      {icon ? (
+        <div className="card__icon-wrapper">
+          <span className="card__photoCount">{photoCount}</span>
+          <FontAwesomeIcon icon={faCameraRetro} size="7x" className="card__icon" />
+        </div> ) : ('')}
     </div>
   );
 };
